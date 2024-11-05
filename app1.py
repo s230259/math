6915eb86744b96757809744d8aff318e9f087dc4
s230259 <gimpo.s230259@ggh.goe.go.kr>
2024-11-05 16:23:57 +0900

    Update app1.py

--- app1.py
+++ app1.py
@@ -9,14 +9,14 @@ st.subheader('오늘의 주제: 양수와 음수')
 st.write('음수?? 어렵지 않아!') 
 
 # 여러 개의 열(문단)을 생성
-#col1, col2 = st.columns(2)       
+col1, col2 = st.columns(2)       
 # 왼쪽 문단
 #with col1:
-#      st.subheader('**개념정리')
-#      st.write('- 양수: 0보다 큰 수')
-#      st.write('- 음수: 0보다 작은 수')
-#      st.write('- 양수: (+) / 음수: (-) 부호가 붙음')
-#      st.write('- 부호: (+)는 생략 가능 /(-)는 생략 불가')
+      st.subheader('**개념정리')
+      st.write('- 양수: 0보다 큰 수')
+      st.write('- 음수: 0보다 작은 수')
+      st.write('- 양수: (+) / 음수: (-) 부호가 붙음')
+      st.write('- 부호: (+)는 생략 가능 /(-)는 생략 불가')
 # 오른쪽 문단
 #with col2:
 #      st.image('______________')  # 이미지 파일 불러오기
